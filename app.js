eddbac2747a4f28f7170f3a20e7eb1782a30dcce
salissadiq <salissadiq@gmail.com>
2020-01-16 10:59:40 +0100

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express()
 require('./db')()
 app.use(cors())
 app.use(express.json())
-////////
+//
+//
+//
 app.use('/auth', auth)
 app.use('/songs', songs)
 app.use('/bookmarks', bookmark)
