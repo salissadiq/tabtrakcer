1f0050967aeb5cc074bce9b268efc2aaf39b1204
salissadiq <salissadiq@gmail.com>
2020-01-16 10:43:57 +0100

    add db supprt

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 module.exports = () => {
-  mongoose.connect('mongodb://localhost/tab_tracker', { useNewUrlParser: true })
+  mongoose.connect('mongodb://sals:t123456@ds263848.mlab.com:63848/tab-tracker', { useNewUrlParser: true })
     .then(console.log('Connected to mongodb'))
     .catch(err => console.log(err))
 }
